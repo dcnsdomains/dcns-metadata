5e2f0b1b2980bcd7cd0071736b8251881af64a81
mainnet-pat <mainnet_pat@protonmail.com>
2022-09-15 14:36:24 +0000

    Change rpc

--- src/network.ts
+++ src/network.ts
@@ -84,7 +84,7 @@ export default function getNetwork(network: string): any {
       break;
     case NETWORK.DOGECHAIN:
       SUBGRAPH_URL = 'https://graph.bch.domains/subgraphs/name/graphprotocol/ens-dogechain';
-      INFURA_URL = `https://rpc.dogmoney.money/public/full`;
+      INFURA_URL = `https://rpc.yodeswap.dog`;
       NETWORKISH = {
         name: "dogechain",
         chainId: 2000,
